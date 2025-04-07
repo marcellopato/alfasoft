1f7794b423f9acf74ea30a4a8d046432c16d7dcf
Marcello Correia <marcellocorreia@gmail.com>
2025-04-07 20:43:40 +0100

    feat: Adiciona layout de aplicativo e atualiza a página de criação de contatos

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,13 +1,20 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Novo Contato') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-    <h1>Novo Contato</h1>
-
-    <form action="{{ route('contacts.store') }}" method="POST">
-        @csrf
-        @include('contacts._form')
-        
-        <button type="submit" class="btn btn-primary">Salvar</button>
-        <a href="{{ route('contacts.index') }}" class="btn btn-secondary">Cancelar</a>
-    </form>
-@endsection
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="{{ route('contacts.store') }}" method="POST">
+                        @csrf
+                        @include('contacts._form')
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
